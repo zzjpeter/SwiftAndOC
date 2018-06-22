29c2efbbd3ef202d7fbbc8aafbb1f84757cfb5c1
zhuzj <2603160657@qq.com>
2018-06-22 18:31:26 +0800

    C 函数

--- SwiftAndOC/CClass.c
+++ SwiftAndOC/CClass.c
@@ -7,3 +7,22 @@
 //
 
 #include "CClass.h"
+
+//1.定义函数
+int sum3(int a, int b)
+{
+    return a + b;
+}
+
+void desc3() {
+    //2.声明函数指针
+    int (*p) (int, int);
+    
+    //3.函数指针指向函数
+    p = sum3;
+    
+    //4.使用
+    int result = p(10, 10);
+    
+    printf("swift调用有头文件的C函数输出:%d\n",result);
+}
--- SwiftAndOC/CClass.h
+++ SwiftAndOC/CClass.h
@@ -10,5 +10,7 @@
 #define CClass_h
 
 #include <stdio.h>
-
+//和OC中类似,在C的头文件中声明两个函数
+int sum3(int a, int b);
+void desc3(void);
 #endif /* CClass_h */
